1973baa41a4fcc7a65c554f54ead099824aa3c2d
sydesoft <31887062+sydesoft@users.noreply.github.com>
2022-04-28 11:14:04 +0200

    add sample application to repo

---
+++ NfcTagWriter/FrmMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace NfcTagWriter
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtData = new System.Windows.Forms.TextBox();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.ChkRead = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(178, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NFC TagWriter";
+            // 
+            // TxtData
+            // 
+            this.TxtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtData.Location = new System.Drawing.Point(12, 99);
+            this.TxtData.Name = "TxtData";
+            this.TxtData.Size = new System.Drawing.Size(579, 29);
+            this.TxtData.TabIndex = 0;
+            // 
+            // LblResult
+            // 
+            this.LblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResult.Location = new System.Drawing.Point(12, 160);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(579, 209);
+            this.LblResult.TabIndex = 4;
+            this.LblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ChkRead
+            // 
+            this.ChkRead.AutoSize = true;
+            this.ChkRead.Location = new System.Drawing.Point(12, 134);
+            this.ChkRead.Name = "ChkRead";
+            this.ChkRead.Size = new System.Drawing.Size(52, 17);
+            this.ChkRead.TabIndex = 5;
+            this.ChkRead.Text = "Read";
+            this.ChkRead.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 378);
+            this.Controls.Add(this.ChkRead);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.TxtData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Sydesoft NFC TagWriter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtData;
+        private System.Windows.Forms.Label LblResult;
+        private System.Windows.Forms.CheckBox ChkRead;
+    }
+}
+
---
+++ NfcTagWriter/FrmMain.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NfcDevice;
+
+namespace NfcTagWriter
+{
+    public partial class FrmMain : Form
+    {
+        private ACR122U acr122u = new ACR122U();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] GetSHA1(byte[] data)
+        {
+            byte[] hash = null;
+
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                hash = sha1.ComputeHash(data);
+            }
+
+            return hash;
+        }
+
+        private byte[] CreateNfcData(byte[] data)
+        {
+            // length of data (4 bytes) + data + sha1 (20 bytes)
+            List<byte> nfcData = new List<byte>();
+            nfcData.AddRange(BitConverter.GetBytes(data.Length));
+            nfcData.AddRange(data);
+            nfcData.AddRange(GetSHA1(data));
+
+            return nfcData.ToArray();
+        }
+
+        private byte[] GetData(byte[] nfcData)
+        {
+            byte[] data = new byte[BitConverter.ToInt32(nfcData, 0)];
+            byte[] sha1 = new byte[20];
+
+            Array.Copy(nfcData, 4, data, 0, data.Length);
+            Array.Copy(nfcData, data.Length + 4, sha1, 0, sha1.Length);
+
+            if (GetSHA1(data).SequenceEqual(sha1))
+                return data;
+            else
+                return null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TxtData.MaxLength = 38;
+
+            acr122u.Init(false, (TxtData.MaxLength * 2) + 20 + 4, 4, 4, 100);  // MaxLength * 2 if using UTF8 + 20 bytes SHA1 + 4 bytes length of data
+            acr122u.CardInserted += Acr122u_CardInserted;
+            acr122u.CardRemoved += Acr122u_CardRemoved;
+        }
+
+        private void Acr122u_CardInserted(PCSC.ICardReader reader)
+        {
+            Invoke((MethodInvoker)delegate ()
+            {
+                LblResult.ForeColor = Color.Black;
+                LblResult.Text = "...";
+            });
+
+            try
+            {
+                if (ChkRead.Checked)
+                {
+                    byte[] data = GetData(acr122u.ReadData(reader));
+
+                    Invoke((MethodInvoker)delegate ()
+                    {
+                        if (data != null)
+                        {
+                            LblResult.ForeColor = Color.Green;
+                            LblResult.Text = "OK";
+
+                            TxtData.Text = Encoding.UTF8.GetString(data);
+                        }
+                        else
+                        {
+                            LblResult.ForeColor = Color.Red;
+                            LblResult.Text = "ERROR";
+
+                            TxtData.Text = string.Empty;
+                        }
+                    });
+                }
+                else
+                {
+                    bool ret = acr122u.WriteData(reader, CreateNfcData(Encoding.UTF8.GetBytes(TxtData.Text)));
+
+                    Invoke((MethodInvoker)delegate ()
+                    {
+                        if (ret)
+                        {
+                            LblResult.ForeColor = Color.Green;
+                            LblResult.Text = "OK";
+                        }
+                        else
+                        {
+                            LblResult.ForeColor = Color.Red;
+                            LblResult.Text = "ERROR";
+                        }
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke((MethodInvoker)delegate ()
+                {
+                    LblResult.ForeColor = Color.Red;
+                    LblResult.Text = ex.Message;
+                });
+            }
+        }
+
+        private void Acr122u_CardRemoved()
+        {
+            Invoke((MethodInvoker)delegate ()
+            {
+                TxtData.Text = string.Empty;
+                LblResult.Text = string.Empty;
+            });
+        }
+    }
+}
